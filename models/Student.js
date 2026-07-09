a3b9154f09c76fc7e5221e8ebf1892752c1dc499
Muhammad Shahid <shahid.mern.stack@gmail>
2026-07-09 05:03:55 -0700

    mongodb connect and create student model

---
+++ models/Student.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    phone: {
+      type: String,
+      required: true
+    },
+
+    course: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Student = mongoose.model("Student", studentSchema);
+
+export default Student;
